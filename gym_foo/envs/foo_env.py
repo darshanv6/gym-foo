2c664e4ce4d1fcb930e849f4c11722068b4ccc46
darshanv6 <darshan.blh@gmail.com>
2019-12-08 23:03:49 +0530

    Added observation and action states

--- gym_foo/envs/foo_env.py
+++ gym_foo/envs/foo_env.py
@@ -5,8 +5,13 @@ from gym.utils import seeding
 class FooEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-    ...
+  def __init__(self, num_containers, avg_mem_utilization):
+    self.action_space = spaces.Discrete(2) #There are two actions: upscale and downscale
+    self.observation_space = spaces.Tuple((
+      spaces.Discrete(TOTAL_NUMBER_OF_CONTAINERS), #total number of containers
+      spaces.Discrete(50), #number of containers being used
+      spaces.Box(np.array([0]), np.array([100]),dtype=np.float16)) #average memory utilization 
+    
   def step(self, action):
     ...
   def reset(self):
