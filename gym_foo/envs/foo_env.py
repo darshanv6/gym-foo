8cecff449cf96d632324a5af6f2b72017114bde3
darshanv6 <darshan.blh@gmail.com>
2019-12-08 23:19:57 +0530

    Added reset function

--- gym_foo/envs/foo_env.py
+++ gym_foo/envs/foo_env.py
@@ -10,12 +10,19 @@ class FooEnv(gym.Env):
     self.observation_space = spaces.Tuple((
       spaces.Discrete(TOTAL_NUMBER_OF_CONTAINERS), #total number of containers
       spaces.Discrete(50), #number of containers being used
-      spaces.Box(np.array([0]), np.array([100]),dtype=np.float16)) #average memory utilization 
+      spaces.Box(np.array([0]), np.array([100]),dtype=np.float16))) #average memory utilization 
     
   def step(self, action):
     ...
   def reset(self):
-    ...
+    self.num_containers = INITIAL_NUMBER_OF_CONTAINERS
+    # Set the current step to a random point within the data frame
+    self.current_step = random.randint(0, len(self.num_containers.loc[:, 'Open'].values) - 6)
+    return self._next_observation()
+
+  def _next_observation(self):
+    
+
   def render(self, mode='human'):
     ...
   def close(self):
