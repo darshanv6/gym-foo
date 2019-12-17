592984d4bab9fcd28e49a3c466fa1e7b3bf23ffa
darshanv6 <darshan.blh@gmail.com>
2019-12-17 23:46:59 +0530

    Errors fixed in environment

--- gym_foo/envs/foo_env.py
+++ gym_foo/envs/foo_env.py
@@ -17,8 +17,8 @@ class FooEnv(gym.Env):
   def __init__(self):
     self.action_space = spaces.Discrete(2) #There are two actions: upscale and downscale
     self.observation_space = spaces.Tuple((
-      spaces.Discrete(TOTAL_NUMBER_OF_CONTAINERS), #total number of containers
-      spaces.Discrete(INITIAL_NUMBER_OF_CONTAINERS), #number of containers being used
+      spaces.Box(low = 0, high = TOTAL_NUMBER_OF_CONTAINERS, shape=(1,TOTAL_NUMBER_OF_CONTAINERS), dtype = np.int8), #total number of containers
+      spaces.Box(low = 0, high = INITIAL_NUMBER_OF_CONTAINERS, shape=(1,INITIAL_NUMBER_OF_CONTAINERS), dtype = np.int8), #number of containers being used
       spaces.Box(low = -1, high = 1,shape=(3,2),dtype=np.int8))) #The range of actions possible (-1 for downscale and +1 for upscale)
     self.total_containers = TOTAL_NUMBER_OF_CONTAINERS
     self.avg_mem_utilization = 20.0
@@ -33,12 +33,14 @@ class FooEnv(gym.Env):
         #if self.current_step > TOTAL_NUMBER_OF_CONTAINERS:
         #self.current_step = 0
 
-        delay_modifier = (self.current_step / MAX_STEPS)
+        delay_modifier = (self.current_step / MAX_STEPS) #delay modifier is epsilon
 
         #Creating the DataFrame 
         #self.df = pd.DataFrame([[0, 0, 3, 0, 1, 0], [0, -3, 3, 3, -1, 0], [0, 0, 0, +3, -1, -1]],
         #index=['S1', 'S2', 'S3'],
         #columns=['S1d', 'S1u', 'S2d', 'S2u', 'S3d', 'S3u'])
+
+        x = 100
 
         if self.avg_mem_utilization < 30.0:
           self.next_state = 'S1'
@@ -47,74 +49,75 @@ class FooEnv(gym.Env):
         elif self.avg_mem_utilization >= 70.0:
           self.next_state = 'S3'
 
+
         if self.next_state == 'S1':
          if self.current_state == 'S1':
-            if self.current_action == 1:
-              reward = 0
-            elif self.current_action == -1:
-              reward = 0
+            if self.current_action == 1: # 1 means upscaling
+              x = 0
+            elif self.current_action == -1: # -1 means downscaling
+              x = 0
         
         if self.next_state == 'S1':
          if self.current_state == 'S2':
             if self.current_action == 1:
-              reward = 0
+              x = 0
             elif self.current_action == -1:
-              reward = 3
+              x = 3
 
         if self.next_state == 'S1':
          if self.current_state == 'S3':
             if self.current_action == 1:
-              reward = 0
+              x = 0
             elif self.current_action == -1:
-              reward = 1
+              x = 1
 
         if self.next_state == 'S2':
          if self.current_state == 'S1':
             if self.current_action == 1:
-              reward = -3
+              x = -3
             elif self.current_action == -1:
-              reward = 0
+              x = 0
 
         if self.next_state == 'S2':
          if self.current_state == 'S2':
             if self.current_action == 1:
-              reward = 3
+              x = 3
             elif self.current_action == -1:
-              reward = 3
+              x = 3
 
         if self.next_state == 'S2':
          if self.current_state == 'S3':
             if self.current_action == 1:
-              reward = 0
+              x = 0
             elif self.current_action == -1:
-              reward = -1
+              x = -1
 
         if self.next_state == 'S3':
          if self.current_state == 'S1':
             if self.current_action == 1:
-              reward = 0
+              x = 0
             elif self.current_action == -1:
-              reward = 0
+              x = 0
 
         if self.next_state == 'S3':
          if self.current_state == 'S2':
             if self.current_action == 1:
-              reward = 3
+              x = 3
             elif self.current_action == -1:
-              reward = 0
+              x = 0
 
         if self.next_state == 'S3':
          if self.current_state == 'S3':
             if self.current_action == 1:
-              reward = -1
+              x = -1
             elif self.current_action == -1:
-              reward = -1
+              x = -1
 
-        reward = reward * delay_modifier
+        reward = x * delay_modifier
 
         self._take_action(action)
 
-        done = self.current_step >= 40
+        done = self.current_step >= MAX_STEPS
 
         obs = self._next_observation()
 
@@ -136,8 +139,9 @@ class FooEnv(gym.Env):
         ])
 
     # Append additional data and scale each value to between 0-1
-    obs = np.append(frame, axis=0)
-    return obs
+    
+    # np.append(obs,frame, axis=0)
+    return frame
 
   def _take_action(self, action):
     if action < 1: #upscaling action
@@ -153,4 +157,10 @@ class FooEnv(gym.Env):
     # Render the environment to the screen
     print(f'Step: {self.current_step}')
     print(f'Average memory utilization: {self.avg_mem_utilization}')
-    print(f'Number of containers: {self.num_containers}')
+    print(f'Total number of containers: {self.total_containers}')
+    print(f'Number of containers in use: {self.num_containers}')
+    print(f'Current state: {self.current_state}')
+    print(f'Next state: {self.next_state}')
+    print(f'Current action: {self.current_action}')
+
+
